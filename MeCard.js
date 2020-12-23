6e177ab37501c4c5ff00231a5884b72d558f4693
RVillanuevaRVC <75404849+RVillanuevaRVC@users.noreply.github.com>
2020-12-23 21:21:57 +0700

    Update MeCard.js

--- MeCard.js
+++ MeCard.js
@@ -106,6 +106,10 @@ class MeCardCtrl extends React.Component
   constructor(props) 
   {
     super(props);
+    this.state = {
+      open : false,
+      favorite : false
+    } ;
     // Don't call this.setState() here!
     //this.state = { counter: 0 };
     //this.handleClick = this.handleClick.bind(this);
@@ -114,12 +118,10 @@ class MeCardCtrl extends React.Component
   render()
   {
   
-    //const [open, setOpen] = React.useState(false);
-    //const [favorite, setFavorite] = React.useState(false);
   
     const ExpandButton = ({ ...rest }) => 
     {
-      const Icon = FormUp;// : FormDown;
+      const Icon = this.state.open ? FormUp : FormDown;
       return (
         <Button
           hoverIndicator="light-4"
@@ -128,40 +130,30 @@ class MeCardCtrl extends React.Component
         />
       );
     };
+    
     return (
-<Box pad="medium" alignContent= "center" align="start" border={true}>
+      <Box pad="medium" alignContent= "center" align="start" border={true}>
         <Card elevation="large" width="large">
           <CardBody height="small">
-            <Image
-              fit="cover"
-              src="//v2.grommet.io/assets/IMG_4245.jpg"
-              a11yTitle="bridge"
-            />
+            <Image fit="cover" src="//v2.grommet.io/assets/IMG_4245.jpg" a11yTitle="bridge"/>
           </CardBody>
+          
           <Box pad={{ horizontal: 'medium' }} responsive={false}>
-            <Heading level="3" margin={{ vertical: 'medium' }}>
-              Bridge
-            </Heading>
+            <Heading level="3" margin={{ vertical: 'medium' }}> Bridge </Heading>
             <Box align="center" pad="large">
-      
-    </Box>
+            </Box>
           </Box>
+          
           <CardFooter>
             <Box direction="row" align="center" gap="small">
-              <Button
-                icon={<Favorite color={'red' } />}
-                hoverIndicator
-                
-              />
+              <Button icon={<Favorite color={'red' } />} hoverIndicator />
               <Button icon={<ShareOption color="plain" />} hoverIndicator />
-              <Anchor
-                href="https://www.collinsdictionary.com/us/dictionary/english/bridge"
-                label="Learn More"
-              />
+              <Anchor href="https://www.collinsdictionary.com/us/dictionary/english/bridge" label="Learn More"/>
             </Box>
-            <ExpandButton  />
+            <ExpandButton  onClick={() => this.setState({open:!this.state.open})}/>
           </CardFooter>
-          <Collapsible >
+          
+          <Collapsible open = {this.state.open}>
             <Paragraph margin="medium" color="dark-3">
               The greatest bridge builders of antiquity were the ancient Romans.The Romans built arch bridges and aqueducts that could stand in conditions that would damage or destroy earlier designs. Some
               stand today.
